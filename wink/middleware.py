ebe8115a605fb5d24852d0a8156a2df729cb3798
Denis <work80@mail.ru>
2025-11-06 12:10:01 +0700

    [dev]: 'wink/middleware.py' was corrected'

--- wink/middleware.py
+++ wink/middleware.py
@@ -1,10 +1,9 @@
 from http.client import responses
 from typing import Any
 
-from django.contrib.auth import (authenticate, login)
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.conf import settings
-
 
 
 class AuthenticationMiddleware:
@@ -12,16 +11,16 @@ class AuthenticationMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        global root_user
         try:
             root_user = User.objects.get(username="root")
         except User.DoesNotExist:
-            root_user = User.objects.create_user(username="root", email="email@email.ru", password="123")
-        finally:
+            root_user = User.objects.create_user(
+                username="root", email="email@email.ru", password="123"
+            )
+        if root_user:
             root_user.is_staff = True
             root_user.is_active = True
             root_user.save()
             request.user = root_user
-        responses = self.get_response(request)
-        return responses
-
+        resp = self.get_response(request)
+        return resp
